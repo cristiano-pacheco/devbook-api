4b3e3b4f709dcec3d17eb58b8863fccd20b74cdb
Cristiano Pacheco <chris.spb25@gmail.com>
2021-05-21 13:41:29 -0300

    database connection

--- main.go
+++ main.go
@@ -10,10 +10,9 @@ import (
 
 func main() {
 	config.Load()
-	fmt.Println("DSN: " + config.DatabaseDSN)
+
 	r := router.Generate()
 
-	address := fmt.Sprintf(":%d", config.ApiPort)
-	fmt.Println(address)
+	fmt.Printf("Listening on port: %d", config.ApiPort)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.ApiPort), r))
 }
---
+++ src/database/database.go
@@ -0,0 +1,20 @@
+package database
+
+import (
+	"api/src/config"
+	"database/sql"
+)
+
+func Connect() (*sql.DB, error) {
+	db, err := sql.Open("mysql", config.DatabaseDSN)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
