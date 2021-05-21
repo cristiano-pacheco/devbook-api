fa4cb9b239b9b18966bcb068367559053f471182
Cristiano Pacheco <chris.spb25@gmail.com>
2021-05-21 14:08:29 -0300

    user repository create method

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -1,6 +1,15 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/database"
+	"api/src/models"
+	"api/src/repositories"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func UserGetAll(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("User Get All"))
@@ -11,7 +20,27 @@ func UserGet(w http.ResponseWriter, r *http.Request) {
 }
 
 func UserCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("User Create"))
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var user models.User
+	if err = json.Unmarshal(body, &user); err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repository := repositories.NewUserRepository(db)
+	userID, err := repository.Create(user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Write([]byte(fmt.Sprintf("ID inserido: %d", userID)))
 }
 
 func UserUpdate(w http.ResponseWriter, r *http.Request) {
---
+++ src/repositories/user.go
@@ -0,0 +1,36 @@
+package repositories
+
+import (
+	"api/src/models"
+	"database/sql"
+)
+
+type user struct {
+	db *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) *user {
+	return &user{db}
+}
+
+func (repository user) Create(user models.User) (uint64, error) {
+	stmt, err := repository.db.Prepare(
+		"insert into users (name, nick, email, password) values (?, ?, ?, ?)",
+	)
+	if err != nil {
+		return 0, err
+	}
+	defer stmt.Close()
+
+	result, err := stmt.Exec(user.Name, user.Nick, user.Email, user.Password)
+	if err != nil {
+		return 0, err
+	}
+
+	lastInsertedID, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return uint64(lastInsertedID), nil
+}
